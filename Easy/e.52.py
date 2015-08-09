520b5f7657c60a36448a9ab39548e893c11816b5
Pekmen <milanpekovic89@gmail.com>
2015-08-09 02:58:02 +0200

    e.52.py

---
+++ Easy/e.52.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+def get_alpha_value(word):
+    return(sum([ord(ch) - ord('a') + 1 for ch in word.lower()]))
+
+def main():
+    wordlist = ["Shoe", "Hat"]
+    sortedlist = sorted(wordlist, key=get_alpha_value)
+    print(sortedlist)
+    
+if __name__ == "__main__":
+    main()
